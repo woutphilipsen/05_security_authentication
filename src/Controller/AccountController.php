0aa5887386eaf9d2ea8fbd289ea5ee006f57f2f9
philipsenwout <philipsenwout@outlook.com>
2019-12-30 15:04:08 +0100

    configuring authentication system IS_AUTHENTICATED_FULLY

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @IsGranted("ROLE_ADMIN")
+ * @IsGranted("ROLE_USER")
  */
 class AccountController extends AbstractController
 {
